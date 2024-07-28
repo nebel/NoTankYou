eaf49c5814bc6888d50fb4696ebffef02bcccbeb
nebel <9887+nebel@users.noreply.github.com>
2024-07-29 07:52:21 +0900

    Reduce calls to GetAddonByName

--- NoTankYou/Classes/ModuleBase.cs
+++ NoTankYou/Classes/ModuleBase.cs
@@ -80,9 +80,12 @@ public abstract unsafe class ModuleBase<T> : ModuleBase, IDisposable where T : M
         ActiveWarningStates.Clear();
 
         if (!Config.Enabled) return;
-        if (NameplateAddon is null) return;
-        if (!NameplateAddon->IsReady) return;
-        if (!NameplateAddon->IsVisible) return;
+
+        var nameplateAddon = NameplateAddon;
+        if (nameplateAddon is null) return;
+        if (!nameplateAddon->IsReady) return;
+        if (!nameplateAddon->IsVisible) return;
+
         if (System.BlacklistController.IsZoneBlacklisted(Service.ClientState.TerritoryType)) return;
         if (System.SystemConfig.WaitUntilDutyStart && Service.Condition.IsBoundByDuty() && !Service.DutyState.IsDutyStarted) return;
         if (Config.DutiesOnly && !Service.Condition.IsBoundByDuty()) return;
